9ac2b4c6fcfd22b33dbc8953a486206c796520e6
DarkEclipse86 <adrianodagostino49@gmail.com>
2024-07-02 02:06:41 +0200

    Changes made

---
+++ src/main/java/net/darkeclipse/chickenparts/ChickenPartsClient.java
@@ -0,0 +1,10 @@
+package net.darkeclipse.chickenparts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChickenPartsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/darkeclipse/chickenparts/ChickenPartsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.darkeclipse.chickenparts;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChickenPartsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
