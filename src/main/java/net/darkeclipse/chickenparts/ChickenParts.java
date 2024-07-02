3ccb27c5d00b532c6c9e97cee71660f35a04a4eb
DarkEclipse86 <adrianodagostino49@gmail.com>
2024-07-02 02:57:38 +0200

    Changes made

--- src/main/java/net/darkeclipse/chickenparts/ChickenParts.java
+++ src/main/java/net/darkeclipse/chickenparts/ChickenParts.java
@@ -1,5 +1,6 @@
 package net.darkeclipse.chickenparts;
 
+import net.darkeclipse.chickenparts.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -17,7 +18,7 @@ public class ChickenParts implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/darkeclipse/chickenparts/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.darkeclipse.chickenparts.item;
+
+import net.darkeclipse.chickenparts.ChickenParts;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item CHICKEN_BONE = registerItem("chicken_bone", new Item(new FabricItemSettings()));
+
+    private static void  addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(CHICKEN_BONE);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(ChickenParts.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        ChickenParts.LOGGER.info("Registering Mod Items for " + ChickenParts.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SEARCH).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
